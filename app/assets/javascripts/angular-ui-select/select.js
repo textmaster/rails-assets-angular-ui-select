be9fab4af871191da86bb4eb1376da9add015077
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2015-08-28 20:38:09 +0300

    get placeholder

--- app/assets/javascripts/angular-ui-select/select.js
+++ app/assets/javascripts/angular-ui-select/select.js
@@ -1075,7 +1075,7 @@ uis.directive('uiSelectMultiple', ['uiSelectMinErr','$timeout', function(uiSelec
 
       ctrl.getPlaceholder = function(){
         //Refactor single?
-        if($select.selected.length) return;
+        // if($select.selected.length) return;
         return $select.placeholder;
       };
 
